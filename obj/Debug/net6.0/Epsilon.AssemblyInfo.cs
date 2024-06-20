a3a397b50e1aed67008e6176f21104658d6af1f7
Yann Lancelin <lancelinyann@gmail.com>
2024-06-20 13:06:08 +0200

    Update README

--- obj/Debug/net6.0/Epsilon.AssemblyInfo.cs
+++ obj/Debug/net6.0/Epsilon.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Use VMware Player or Workstation to deploy and debug.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f893543a7c74d284f331c81f11d6a4d98f6557b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+feabc3e02142f2998fb61d463751e5bafe058219")]
 [assembly: System.Reflection.AssemblyProductAttribute("Epsilon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Epsilon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
